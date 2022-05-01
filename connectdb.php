bcfc226425fc04ca3fb1ec312422236e80296383
Rodney_RWR <85048343+Rodney-RWR@users.noreply.github.com>
2022-05-02 09:34:07 +1000

    Create connectdb.php

---
+++ connectdb.php
@@ -0,0 +1,11 @@
+    <?php
+
+//Create Connnection
+$sqlLink = mysqli_connect('bavtsffrbfljayi5qlzx-mysql.services.clever-cloud.com', 'unofmuc1gboteq65', 'yewg0X4vz0EqLdM1e4EB', 'bavtsffrbfljayi5qlzx');
+
+//If Error Connecting
+if(!$sqlLink) {
+    die('<center><br><h3>Error connecting to servers Database.');
+}
+
+?>
